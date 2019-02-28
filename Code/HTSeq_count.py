b59a126557d9de55f44b3a90142e9d715326952f
davidgllund <student7@ip-172-31-32-224.eu-west-3.compute.internal>
2019-02-28 18:43:58 +0000

    Fixed bugs, changed variable names

--- Code/HTSeq_count.py
+++ Code/HTSeq_count.py
@@ -1,45 +1,47 @@
 #!/usr/bin/python3
 
+# This script takes the SAM files generated from the bowtie2 alignment and the GTF gene annotation file that was downloaded from ensembl and generates a count matrix.
+# NOTE: This script should be run from the "Intermediate" directory.
+
 import HTSeq
 import numpy as np
 import sys
 
 # Import the SAM-files for all 6 samples
-SAM_Sample1 = HTSeq.SAM_Reader("Intermediate/Sample1.sam")
-SAM_Sample2 = HTSeq.SAM_Reader("Intermediate/Sample2.sam")
-SAM_Sample3 = HTSeq.SAM_Reader("Intermediate/Sample3.sam")
-SAM_Sample4 = HTSeq.SAM_Reader("Intermediate/Sample4.sam")
-SAM_Sample5 = HTSeq.SAM_Reader("Intermediate/Sample5.sam")
-SAM_Sample6 = HTSeq.SAM_Reader("Intermediate/Sample6.sam")
+samSample1 = HTSeq.SAM_Reader("Sam_files/Sample1.sam")
+samSample2 = HTSeq.SAM_Reader("Sam_files/Sample2.sam")
+samSample3 = HTSeq.SAM_Reader("Sam_files/Sample3.sam")
+samSample4 = HTSeq.SAM_Reader("Sam_files/Sample4.sam")
+samSample5 = HTSeq.SAM_Reader("Sam_files/Sample5.sam")
+samSample6 = HTSeq.SAM_Reader("Sam_files/Sample6.sam")
 
 # Import the GTF annotation file
-GTF_File = HTSeq.GFF_Reader("Data/Gene_annotation.gtf.gz")
+geneAnnotation = HTSeq.GFF_Reader("../Data/Reference_data/GeneAnnotation.gtf.gz")
 
 # Generate an array containing the annotated exons. Since the reads are strand specific the argument 'stranded = True' is used
 exons = HTSeq.GenomicArrayOfSets("auto", stranded = True)
 
-for feature in GTF_File:
+for feature in geneAnnotation:
     if feature.type == "exon":
         exons[feature.iv] += feature.name
 
 # Find the intersections of all overlapping exons
-iset = None
-for iv2, step_set in exons[iv].steps():
-    if iset is None:
-        iset = step_set.copy()
-    else:
-        iset.intersection_update(step_set)
+    iset = None
+    for iv2, step_set in exons[feature.iv].steps():
+        if iset is None:
+            iset = step_set.copy()
+        else:
+            iset.intersection_update(step_set)
 
 # Go through all of the reads, and, if it contains a single gene name, add a count for that gene
 counts = {}
-for feature in GTF_File:
+for feature in geneAnnotation:
     if feature.type == "exon":
         counts[feature.name] = 0
-        
-# Function that, given a SAM-file, counts the reads and returns a vector of gene counts for that sample
-def count_reads(SAM_file):
-# Count the reads in the SAM-file
-    for alnmt in SAM_file:
+
+# Function that, given a SAM-file, counts the reads and returns a vector of gene counts for the corresponding sample
+def count_reads(sam_file):
+    for alnmt in sam_file:
         if alnmt.aligned:
             iset = None
             for iv2, step_set in exons[alnmt.iv].steps():
@@ -65,12 +67,12 @@ def count_reads(SAM_file):
     return countVector
 
 # Use the function to generate count vectors for all samples
-countVector1 = count_reads(SAM_Sample1)
-countVector2 = count_reads(SAM_Sample2)
-countVector3 = count_reads(SAM_Sample3)
-countVector4 = count_reads(SAM_Sample4)
-countVector5 = count_reads(SAM_Sample5)
-countVector6 = count_reads(SAM_Sample6)
+countVector1 = count_reads(samSample1)
+countVector2 = count_reads(samSample2)
+countVector3 = count_reads(samSample3)
+countVector4 = count_reads(samSample4)
+countVector5 = count_reads(samSample5)
+countVector6 = count_reads(samSample6)
 
 # Generate the count matrix by concatenating the vectors
 countMatrix = np.concatenate((countVector1, countVector2), axis = 1)
